287e73bb309879dbecc9835960547f64759a59f0
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-05-06 12:10:03 +0300

    Optimize imports

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 '''Train CIFAR10 with PyTorch.'''
 from __future__ import print_function
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
+import argparse
+import os
 
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-
-import os
-import argparse
+from torch.utils.data import DataLoader
 
 from models import *
 from utils import progress_bar
-from torch.autograd import Variable
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
