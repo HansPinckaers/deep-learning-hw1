cb378827d09939c76288bab76bd85514713d3096
Hans Pinckaers <hans.pinckaers@gmail.com>
2018-07-28 21:05:57 +0200

    some testing

--- models/resnet.py
+++ models/resnet.py
@@ -9,16 +9,61 @@ Reference:
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
+class RandomZeroPadding(torch.nn.Module):
+    def __init__(self, padding):
+        super(RandomZeroPadding, self).__init__()
+        self.padding = padding
+        self.mask = None
+
+    def forward(self, input):
+        padding = self.padding
+
+        def pad_rows(arr, prev_mask=None):
+            mask = torch.ones((input.shape[1], input.shape[2], input.shape[3] + padding * 2),
+                              requires_grad=False, dtype=torch.uint8).cuda()
+            mask[:, :, 0:padding * 2] = 0  # set first two columns on zero
+            # Randomize zeroes per row
+            n_cols = mask.shape[2]
+            for i in range(mask.shape[1]):
+                mask[:, i] = mask[:, i, torch.randperm(n_cols)]
+            output = torch.zeros(input.shape[0], input.shape[1], input.shape[2],
+                                 input.shape[3] + padding * 2, dtype=input.dtype).cuda()
+            output.masked_scatter_(mask, input)
+            if prev_mask is not None:
+                mask_output = torch.zeros(prev_mask.shape[0], prev_mask.shape[1],
+                                          prev_mask.shape[2] + padding * 2, dtype=prev_mask.dtype).cuda()
+                mask_output.masked_scatter_(mask[0], prev_mask)
+                mask = mask_output
+            return output, mask
+
+        input, mask = pad_rows(input)
+
+        # transpose
+        input = input.transpose(2, 3)
+        mask = mask.transpose(1, 2)
+        input, mask = pad_rows(input, mask)
+
+        # transpose back
+        input = input.transpose(2, 3)
+        self.mask = mask.transpose(1, 2)
+
+        return input
+
+    def backward(self, grad_output):
+        return grad_output.masked_select(self.mask)
 
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=0, bias=False)
+        self.pad1 = RandomZeroPadding(1)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=0, bias=False)
+        self.pad2 = RandomZeroPadding(1)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
@@ -29,7 +74,9 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.pad1(x)
+        out = F.relu(self.bn1(self.conv1(out)))
+        out = self.pad2(out)
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
         out = F.relu(out)
@@ -43,7 +90,8 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=0, bias=False)
+        self.pad1 = RandomZeroPadding(1)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
@@ -57,6 +105,7 @@ class Bottleneck(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
+        out = self.pad1(out)
         out = F.relu(self.bn2(self.conv2(out)))
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
@@ -69,7 +118,8 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=0, bias=False)
+        self.pad1 = RandomZeroPadding(1)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -86,7 +136,8 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.pad1(x)
+        out = F.relu(self.bn1(self.conv1(out)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
